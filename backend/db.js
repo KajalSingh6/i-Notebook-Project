f5cbcb540b049d0e49e37fadcc73dca210264a4d
KajalSingh6 <kajalsingh5681@gmail.com>
2025-03-10 13:06:23 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook";
+// const mongoURI = "mongodb://localhost:27017/inotebook";
+// const mongoURI = "mongodb+srv://kajalsingh82793:ZMDNx3bF6TV2TPCX@cluster0.x4kif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI);
